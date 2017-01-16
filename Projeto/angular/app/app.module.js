903979efadb4173af65e26d02320820505c2879e
Unknown <alexajosue@hotmail.com>
2017-01-16 20:58:44 +0000

    Small changes - Debug

--- Projeto/angular/app/app.module.js
+++ Projeto/angular/app/app.module.js
@@ -11,12 +11,14 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
 var app_component_1 = require('./app.component');
+var authentication_module_1 = require("./authentication/authentication.module");
+var app_routing_module_1 = require("./app.routing.module");
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule],
+            imports: [platform_browser_1.BrowserModule, authentication_module_1.AuthenticationModule, app_routing_module_1.AppRoutingModule],
             declarations: [app_component_1.AppComponent],
             bootstrap: [app_component_1.AppComponent]
         }), 
--- Projeto/angular/app/app.module.ts
+++ Projeto/angular/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent }   from './app.component';
+import {AuthenticationModule} from "./authentication/authentication.module";
+import {AppRoutingModule} from "./app.routing.module";
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, AuthenticationModule, AppRoutingModule],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
